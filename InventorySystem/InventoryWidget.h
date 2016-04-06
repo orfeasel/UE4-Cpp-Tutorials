662c5ab5d5539acbba036988dcbcb6a0b3ea6401
Orfeas Eleftheriou <orfeasel@users.noreply.github.com>
2016-04-06 14:02:20 +0300

    InventoryWidget refactoring

--- InventorySystem/InventoryWidget.h
+++ InventorySystem/InventoryWidget.h
@@ -14,32 +14,15 @@ class INVENTORYSYSTEM_API UInventoryWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
-	
-protected:
-	/*True if any animation is playing in order to have smooth UI changes (non overlapping animations)*/
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Animations)
-	bool bIsAnimationPlaying = false;
-
 
 
 public:
-	/*Adds the widget into the viewport and plays its entry animation*/
+	/*Adds the widget into the viewport and populates the inventory slots*/
 	UFUNCTION(BlueprintImplementableEvent, Category = Animations)
 	void Show();
-
-	/*Removes the widget from the viewport and plays the outro animation*/
-	UFUNCTION(BlueprintImplementableEvent, Category = Animations)
-	void Hide();
 
 	/*Stores a reference in order to bind information on inventory slots*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TArray<APickup*> ItemsArray;
 
-	/*True if any animation is playing -entry or outro-*/
-	FORCEINLINE bool GetIsAnyAnimationActive() { return bIsAnimationPlaying; }
-
-	/*Resets the equip status on all inventory slots*/
-	UFUNCTION(BlueprintImplementableEvent, Category = UI)
-	void ResetInventorySlotEquipStatus();
-	
 };
